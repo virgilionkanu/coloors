40d5ff4050f5832f769a78e215a28ae237f8c2d8
Virgílio Cano <virgiliocano53@gmail.com>
2020-07-10 17:41:58 -0300

    Generate first color

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
- 
+//Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+//Functions
+
+//Color Generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hextText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hextText.innerText = randomColor;
+
+    //Check for contrast
+    checkTextContrast(randomColor, hextText);
+  });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+randomColors();
