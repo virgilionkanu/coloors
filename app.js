a7ae25b079319d2b122767f8e13f052dce6b6cc8
Virgílio Cano <virgiliocano53@gmail.com>
2020-07-12 22:51:07 -0300

    Pushing to Local Storage

--- app.js
+++ app.js
@@ -237,6 +237,7 @@ const saveInput = document.querySelector(".save-container input");
 //Event Listeners
 saveBtn.addEventListener("click", openPalette);
 closeSave.addEventListener("click", closePalette);
+submitSave.addEventListener("click", savePalette);
 
 function openPalette(e) {
   const popup = saveContainer.children[0];
@@ -248,6 +249,34 @@ function closePalette(e) {
   const popup = saveContainer.children[0];
   saveContainer.classList.remove("active");
   popup.classList.remove("active");
+}
+
+function savePalette(e) {
+  saveContainer.classList.remove("active");
+  popup.classList.remove("active");
+  const name = saveInput.value;
+  const colors = [];
+  currentHexes.forEach((hex) => {
+    colors.push(hex.innerText);
+  });
+  //Generate Object
+  let paletteNr = savedPalettes.length;
+  const palleteObj = { name, colors, nr: paletteNr };
+  savedPalettes.push(palleteObj);
+  //Save to local storage
+  saveToLocal(palleteObj);
+  saveInput.value = "";
+}
+
+function saveToLocal(paletteObj) {
+  let localPalettes;
+  if (localStorage.getItem("palettes") === null) {
+    localPalettes = [];
+  } else {
+    localPalettes = JSON.parse(localStorage.getItem("palettes"));
+  }
+  localPalettes.push(paletteObj);
+  localStorage.setItem("palettes", JSON.stringify(localPalettes));
 }
 
 randomColors();
