fcc7c55daab804b2654ae3202c95128f810e60a6
Virgílio Cano <virgiliocano53@gmail.com>
2020-07-12 22:25:15 -0300

    Pop up save palette

--- app.js
+++ app.js
@@ -227,4 +227,27 @@ function lockLayer(e, index) {
   }
 }
 
+//Implement Save to palette and Local Storage
+const saveBtn = document.querySelector(".save");
+const submitSave = document.querySelector(".submit-save");
+const closeSave = document.querySelector(".close-save");
+const saveContainer = document.querySelector(".save-container");
+const saveInput = document.querySelector(".save-container input");
+
+//Event Listeners
+saveBtn.addEventListener("click", openPalette);
+closeSave.addEventListener("click", closePalette);
+
+function openPalette(e) {
+  const popup = saveContainer.children[0];
+  saveContainer.classList.add("active");
+  popup.classList.add("active");
+}
+
+function closePalette(e) {
+  const popup = saveContainer.children[0];
+  saveContainer.classList.remove("active");
+  popup.classList.remove("active");
+}
+
 randomColors();
